0508dddd66753df20a608ecd7c0a228cb1bd7b7a
Michel <michelll1307@uni9.edu.br>
2025-04-04 18:05:44 -0300

    feature contagem de estoque criada

--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from produtos.views import ProdutosListView, CadProdutoCreateView, ProdutosDetailView, ProdutoUpdateView, ProdutoDeleteView
 from accounts.views import register_view, login_view, logout_view
+from produtos.views import estoque_view
 
 
 urlpatterns = [
@@ -16,4 +17,5 @@ urlpatterns = [
     path('produto/<int:pk>/', ProdutosDetailView.as_view(), name='prod_detail'),
     path('produto/<int:pk>/update/', ProdutoUpdateView.as_view(), name='prod_update'),
     path('produto/<int:pk>/delete/', ProdutoDeleteView.as_view(), name='prod_delete'),
+    path('estoque/', estoque_view, name='estoque'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
